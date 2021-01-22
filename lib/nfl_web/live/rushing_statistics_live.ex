1685264b3436ddc236a498132dea6b4958555fb5
Thiago <thiago.xth1@gmail.com>
2021-01-22 08:59:08 -0700

    UI updates

--- lib/nfl_web/live/rushing_statistics_live.ex
+++ lib/nfl_web/live/rushing_statistics_live.ex
@@ -3,13 +3,25 @@ defmodule NflWeb.RushingStatisticsLive do
   alias Nfl.Repo
   alias Nfl.Player.RushingStatistics
   import Ecto.Query, only: [from: 2]
-  # def render(assigns) do
-  #   ~L"""
-  #   Current statistics: <%= @statistics[:name] %>
-  #   """
-  # end
+
   def mount(_params, %{}, socket) do
-    query = from s in RushingStatistics, select: %{name: s.name, avg: s.avg}
+    query = from s in RushingStatistics, select: %{
+      att: s.att,
+      att_g: s.att_g,
+      avg: s.avg,
+      first_down: s.first_down,
+      first_down_pct: s.first_down_pct,
+      forty_plus_yds: s.forty_plus_yds,
+      fum: s.fum,
+      lng: s.lng,
+      name: s.name,
+      position: s.position,
+      team: s.team,
+      td: s.td,
+      twenty_plus_yds: s.twenty_plus_yds,
+      yds: s.yds,
+      yds_g: s.yds_g
+    }
     statistics = Repo.all(query)
 
     IO.inspect statistics, label: "statistics"
